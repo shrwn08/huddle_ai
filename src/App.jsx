f29809c718de9d0b0da360e02a09be6f518b75a2
Shrawan <shrwnkumar08@gmail.com>
2026-09-23 11:20:23 +0530

    Initial commit: HuddleAI frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <div className="bg-[linear-gradient(to_top_right,#C3D8F1_0%,#C3D8F1_15%,#F4F6F8_35%,#F4F6F8_65%,#C3D8F1_85%,#C3D8F1_100%)] w-full min-h-screen">
+      APP
+    </div>
+  )
+}
+
+export default App
