7c429647f1f60d523ae6b690812fa6e0e9ce211c
Shrawan <shrwnkumar08@gmail.com>
2026-09-26 23:24:15 +0530

    home page completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import InfiniteCoverFlow from './hero/InfiniteCoverFlow'
+import BottomHero from './hero/BottomHero'
 
 function Hero() {
   return (
-    <div className='w-full h-1/2  flex justify-center items-center'>
+    <div className='w-full h-1/2 flex-col flex gap-5 justify-center items-center'>
         <InfiniteCoverFlow />
-        
+        <BottomHero />
     </div>
   )
 }
--- src/components/hero/BottomHero.jsx
+++ src/components/hero/BottomHero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import bot from "../../assets/chatbot.png"
 
 function BottomHero() {
   return (
-    <div className='w-full h-auto bg-amber-500'>BottomHero</div>
-  )
+    <div className="w-full h-auto  flex flex-col justify-center items-center gap-3 relative">
+      <div className="text-2xl lg:text-4xl font-bold flex flex-col justify-center items-center">
+        <h1 className="">Meet Ashvin - your AI teammate</h1>
+        <h1>Alway stay in the loop.</h1>
+      </div>
+      <div className="text-sm lg:text-md flex flex-col justify-center items-center mt-2">
+        <h2>Ground your answer in pinned context, summarize long threads.</h2>
+        <h2>and create tasks from conversion with Ashvin.</h2>
+      </div>
+      <div className="w-60 h-10 bg-[#0291fA] rounded-md">
+        <button type="button" disabled className="w-full h-full text-white font-semibold cursor-pointer">Invite Ashvin to your channal</button>
+      </div>
+      <div className="hidden  rounded-md border-2 border-zinc-300 w-56 h-20 lg:flex gap-1 absolute -bottom-5 right-5">
+        <div className="h-8 w-8 mt-1">
+        <img src={bot} alt="bot"/>
+        </div>
+        <div className="h-16 w-44 bg-zinc-300 rounded-b-md p-1.5">
+          <p className="text-sm font-semibold">Ashvin</p>
+          <p className="text-[9px] text-justify">Win active chat ground your summarize long treads and create tasks  from with this concept.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BottomHero
+export default BottomHero;
